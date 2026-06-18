0efd4e68d0da5ef01e537d7ffe9716bd94595478
bnf0533-cyber <bnf0533@gmail.com>
2026-06-18 16:05:20 +0300

    fix pydantk file

--- pydantk_vld.py
+++ pydantk_vld.py
@@ -1,25 +1,24 @@
 from pydantic import BaseModel
 from typing import Literal
-Rank = Literal["""
-        Junior,
-        Senior,
-        Commander
-        """]
+
+rank = Literal["Junior","Senior","Commander"]
+
 class CheckAagent(BaseModel):
-    id : int
+    id : int | None = None
     name : str
-    spectialy : str
-    is_active : bool
-    completed_mission : int
-    failed_mission : int
-    agent_rank : Rank
+    specialty : str
+    is_active : bool = True
+    completed_missions : int = 0
+    failed_missions : int = 0
+    agent_rank : rank
 
 class CheckMission(BaseModel):
-    id : int
+    id : int | None = None
     title : str
     description : str
     location : str
     difficulty : int
     importance : int
-    status : Literal["NEW","ASSIGNED","IN_PROGRESS","COMPLETED","FAILED","CANCELLED"]
-    assigned_agens_id : int
+    status : Literal["NEW","ASSIGNED","IN_PROGRESS","COMPLETED","FAILED","CANCELLED"] = "NEW"
+    risk_level : str | None = None
+    assigned_agent_id : int | None = None
